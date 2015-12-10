ec92351d227057529400d13c7ccdd94589a8ca28
pokotyamu <pokotyamu@gmail.com>
2015-12-11 01:25:51 +0900

    ベタ打ちデータグラフの複数表示

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,14 +4,17 @@ class ChartsController < ApplicationController
         @start_at = @end_at - 6
         @categories = @start_at.upto(@end_at).to_a
         @data = [5, 6, 3, 1, 2, 4, 7]
-
-    @h = LazyHighCharts::HighChart.new("graph") do |f|
-      f.chart(:type => "column")
-      f.title(:text => "Sample graph")
-      f.xAxis(:categories => @categories)
-      f.series(:name => "sample",
-        :data => @data)
-      f.export(:type=> 'image/jpeg')
+    @h = []
+    3.times do |i|
+      h = LazyHighCharts::HighChart.new("graph") do |f|
+        f.chart(:type => "column")
+        f.title(:text => "Sample graph#{i}")
+        f.xAxis(:categories => @categories)
+        f.series(:name => "sample",
+          :data => @data)
+        f.export(:type=> 'image/jpeg')
+      end
+      @h << h
     end
   end
 end
