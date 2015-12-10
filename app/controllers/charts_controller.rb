df1b3d159cf3fee25ddb5ea19cb9baa7dd23b054
pokotyamu <pokotyamu@gmail.com>
2015-12-11 06:36:55 +0900

    jsonファイルからグラフを作成する．複数グラフを出力可能．

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,39 +1,29 @@
 # -*- coding: utf-8 -*-
+require 'json'
 
 class ChartsController < ApplicationController
   protect_from_forgery except: :charts_action
-
+  @@charts = []
+  
   def show
-    end_at = Date.today
-    start_at = end_at - 6
-    categories = start_at.upto(end_at).to_a
-    data = [5, 6, 3, 1, 2, 4, 7]
-    data2 = [1, 9, 3, 7, 3, 6, 10]
-    @h = []
-
-    3.times do |i| 
-      h = LazyHighCharts::HighChart.new("graph") do |f|
-        f.title(:text => "Sample graph#{i}")
-        f.chart(:type => "column")
-        f.xAxis(:categories => categories)
-        f.series(:name => "sample", :data => data)
-        f.series(:name => "sample2", :data => data2)
-      end
-      @h << h
-    end
+    @charts = @@charts
+    @@charts = [] #一度出力したら初期化
   end
 
   def create
-    puts params[:json] #jsonデータを受け取り，グラフデータを作成
-    @@hoge = params[:json] #最悪クラス変数にしてしまうのもあり
+    open(params[:j].tempfile) do |io| #jsonファイルの読み取り
+      @json = JSON.load(io)      
+    end
+
+    @json.each do |key,value|
+      h = LazyHighCharts::HighChart.new("graph") do |f|
+        f.title(:text => value["title"])
+        f.chart(:type => value["type"])
+        f.xAxis({title: {text: value["xAxis"]["title"]["text"], margin: 20}, categories: value["xAxis"]["categories"]})
+        f.series(:name => value["series"]["name"] , :data => value["series"]["data"])
+      end
+      @@charts << h
+    end
     redirect_to :action => "show"
   end
-
-
-
-
-
-
-
-  
 end
