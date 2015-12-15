5ad9b8cb63cd20af1aa36f73e5c92967de943df3
pokotyamu <pokotyamu@gmail.com>
2015-12-16 07:29:03 +0900

    heroku用の調整

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   get 'charts/show'
-  post 'charts/create'
+  get 'charts/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
