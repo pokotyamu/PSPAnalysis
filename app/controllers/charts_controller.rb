0e22b5aa64f77e9cd6bbd7bf39651cab064e0d00
pokotyamu <pokotyamu@gmail.com>
2016-01-12 04:28:33 +0900

    テスト環境から本番仕様に変更

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -9,40 +9,21 @@ class ChartsController < ApplicationController
 
   @@charts = []
   def show
-    response = open("http://localhost:5000/result")
-    data = response.read
-    puts data
-    @json = JSON.parse(data)
-    @json.each do |key,value|
-      h = LazyHighCharts::HighChart.new("graph") do |f|
-        f = create_graph(f,value)
-        #戻り値の関係なのか，最後にtype指定しないと上手くグラフtypeが切り替わらない．
-        f.chart(:type => value["type"])
-      end
-      @@charts << h
-    end
-
     @charts = @@charts
     @@charts = [] #一度出力したら初期化
 
   end
 
   def create
-    response = open("http://localhost:5000/result")
-    #response = open('https://java-psp-analysis.herokuapp.com/result')
+    #response = open("http://localhost:5000/result")
+    response = open('https://java-psp-analysis.herokuapp.com/result')
     data = response.read
-    puts data
     @json = JSON.parse(data)
     @json.each do |key,value|
       h = LazyHighCharts::HighChart.new("graph") do |f|
-        create_graph(f,value)
+        f = create_graph(f,value)
+        #戻り値の関係なのか，最後にtype指定しないと上手くグラフtypeが切り替わらない．
         f.chart(:type => value["type"])
-        f.xAxis({title: {text: value["xAxis"]["title"], margin: 20}, categories: value["xAxis"]["categories"]})
-        f.yAxis({title: {text: value["yAxis"]["title"], margin: 20}})
-        value["yAxis"]["series"].each do |se|
-          f.series(:name => se["name"] , :data => se["data"])          
-        end
-        puts f.chart
       end
       @@charts << h
     end
