fc6843aeea11a45adf083f0bf2dd7b9f0aff34ec
pokotyamu <pokotyamu@gmail.com>
2016-01-12 02:50:38 +0900

    折れ線，棒，円グラフそれぞれの基本的なJSONからグラフ作成処理

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,26 +4,45 @@ require 'json'
 
 class ChartsController < ApplicationController
   protect_from_forgery :except => [:create] #:charts_action
-  @@charts = []
-  
-  def show
-    @charts = @@charts
-    @@charts = [] #一度出力したら初期化
-  end
 
-  def create
-    response = open('https://java-psp-analysis.herokuapp.com/result')
+  include IdentifyGraph
+
+  @@charts = []
+  def show
+    response = open("http://localhost:5000/result")
     data = response.read
     puts data
     @json = JSON.parse(data)
     @json.each do |key,value|
       h = LazyHighCharts::HighChart.new("graph") do |f|
-        f.title(:text => value["title"])
+        f = create_graph(f,value)
+        #戻り値の関係なのか，最後にtype指定しないと上手くグラフtypeが切り替わらない．
         f.chart(:type => value["type"])
-        f.xAxis({title: {text: value["xAxis"]["title"]["text"], margin: 20}, categories: value["xAxis"]["categories"]})
-        value["xAxis"]["series"].each do |se|
+      end
+      @@charts << h
+    end
+
+    @charts = @@charts
+    @@charts = [] #一度出力したら初期化
+
+  end
+
+  def create
+    response = open("http://localhost:5000/result")
+    #response = open('https://java-psp-analysis.herokuapp.com/result')
+    data = response.read
+    puts data
+    @json = JSON.parse(data)
+    @json.each do |key,value|
+      h = LazyHighCharts::HighChart.new("graph") do |f|
+        create_graph(f,value)
+        f.chart(:type => value["type"])
+        f.xAxis({title: {text: value["xAxis"]["title"], margin: 20}, categories: value["xAxis"]["categories"]})
+        f.yAxis({title: {text: value["yAxis"]["title"], margin: 20}})
+        value["yAxis"]["series"].each do |se|
           f.series(:name => se["name"] , :data => se["data"])          
         end
+        puts f.chart
       end
       @@charts << h
     end
---
+++ lib/identify_graph.rb
@@ -0,0 +1,41 @@
+module IdentifyGraph
+  def create_graph(f, value)
+    f.title(:text => value["title"])
+
+    case value["type"]
+    when "line" then
+      line_graph(f,value)
+    when "column" then
+      column_graph(f,value)
+    when "pie" then
+      pie_graph(f,value)
+    else
+       puts "hoge"
+    end
+    
+    return f
+  end
+
+  def line_graph(f,value)
+    f.xAxis({title: {text: value["xAxis"]["title"], margin: 20}, categories: value["xAxis"]["categories"]})
+    f.yAxis({title: {text: value["yAxis"]["title"], margin: 20}})
+    value["yAxis"]["series"].each do |se|
+      f.series(:name => se["name"] , :data => se["data"])
+    end
+  end
+
+  def column_graph(f,value)
+    line_graph(f,value)
+    f.plotOptions(column: {stacking: value["yAxis"]["plotOptions"]["column"]["stacking"]})
+  end
+
+  def pie_graph(f, value)
+    data = []
+    value["series"].each do |s|
+      s["data"].each do |d|
+        data << [d["name"],d["y"].pop]
+      end
+      f.series(:name => s["name"],:data => data)
+    end
+  end
+end
